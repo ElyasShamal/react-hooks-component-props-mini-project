76104aa91977d08859f45be7ae2c249eced213d4
Elyas <Elyas.shamal2@gmail.com>
2023-05-28 20:46:32 -0400

    component-props-mini-project

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import Header from "./Header";
 import blogData from "../data/blog";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      <Header name={blogData.name}/>
-      <About image={blogData.image} about={blogData.about}/>
-      
+      <Header name={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
+      <ArticleList posts={blogData.posts} />
+
     </div>
   );
 }
